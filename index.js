542672df4e58ae0ad941c26c2793d349f52212d0
Maegan Wilson <maegan@maeganwilson.com>
2018-05-18 07:23:23 -0500

    starting git repo

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+const app = express();
+require("dotenv").config();
+const DARK_SKY_URL =
+  "https://api.darksky.net/forecast/" + process.env.DARK_SKY_SECRET_KEY + "/";
+
+const GMAP_API_URL =
+  "https://maps.googleapis.com/maps/api/geocode/json?address=";
+const port = 3001;
+
+app.all("/*", function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/api/currenttemp/:longlat", (req, res) => {
+  console.log("works");
+  let location = req.params.longlat;
+  axios
+    .get(DARK_SKY_URL + location)
+    .then(response => {
+      res.send(response.data.currently);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+app.get("/api/daily/:longlat", (req, res) => {
+  let location = req.params.longlat + "?exclude=currently";
+  axios
+    .get(DARK_SKY_URL + location)
+    .then(response => {
+      res.send(response.data.daily);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+app.listen(port, () => {
+  console.log("we are live on " + port);
+});
