857c52f06e8118b016552c37d1cd6c126d12dad4
Maegan Wilson <maegan@maeganwilson.com>
2018-05-21 16:40:15 -0500

    trying to restrict access

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function ignoreFavicon(req, res, next) {
 app.all("/*", function(req, res, next) {
   res.header(
     "Access-Control-Allow-Origin",
-    "http://weather-app.maeganwilson.com/"
+    "http://weather-app.maeganwilson.com"
   );
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
